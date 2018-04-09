710d452c4f01ea3841bf68fc9fba8cafbdaf024e
Cemal Biryol <cbbiryol@cemalsmbook1774.attlocal.net>
2018-04-09 17:23:01 -0400

    Done with 1 of the Challenges

--- assets/JS/sch.js
+++ assets/JS/sch.js
@@ -1,4 +1,7 @@
 //Button for adding Trains
+var key=[];
+var minsTo=[];
+var nextTm=[];
 var config = {
   apiKey: "AIzaSyB8lP7rq2q9ME-ul5NHaao6dvX4XBJHd9E",
   authDomain: "hw-8-bussch.firebaseapp.com",
@@ -33,34 +36,87 @@ $("#add-bus-btn").on("click", function (event) {
       dest: busDest,
       freq: busFreq,
       first: busFirst,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
+      dateChanged: firebase.database.ServerValue.TIMESTAMP
     })
   }
 
 });
 
 database.ref().on("child_added", function (snap) {
+  setTable(snap)
+})
+
+function setTable(snap) {
   var busName = snap.val().name;
   var busDest = snap.val().dest;
   var busFreq = snap.val().freq;
   var busFirst = snap.val().first;
   var timeDiff = moment(moment(), "HH:mm").diff(moment(busFirst, "HH:mm"), 'minutes');
   if (timeDiff < 0) { // if time is past 00:00 (midnight)
-    timeDiff = 24 * 60 + timeDiff -1;
+    timeDiff = 24 * 60 + timeDiff - 1;
   }
   var minutes = timeDiff % busFreq; // this shows the minutes after previous bus
   minutes = busFreq - minutes; // subtract from frequency to obtain minutes to next bus
   var timeOfNext = moment(moment(), "HH:mm").add(minutes, 'minutes').format("HH:mm")
   //Check if it is past 01:00 am (bus service stops at 01:00)
-  if (moment(timeOfNext, "HH:mm").isBetween(moment("01:00","HH:mm"),moment(busFirst, "HH:mm"))) {
-    $("#bus-table > tbody").append("<tr><td>" + busName + "</td><td>" + busDest + "</td><td>" +
-      busFreq + "</td><td>" + "No Bus after 1 am" + "</td><td>" + "First Bus at " + busFirst + "</td></tr>");
+  if (moment(timeOfNext, "HH:mm").isBetween(moment("01:00", "HH:mm"), moment(busFirst, "HH:mm"))) {
+    $("#bus-table > tbody").append("<tr><td>" + busName + "</td><td>" + 
+    busDest + "</td><td>" +
+      busFreq + "</td><td class='minsTo'>" +
+       "No Bus after 1 am" + "</td><td class='nextTm'>" + 
+       "First Bus at " + busFirst + "</td><td>" + 
+       "<button data=" + snap.key + " class='btn btn-success material-icons btnUpdate'>update</button>" + "</td><td>" + 
+       "<button data=" + snap.key + " class='btn btn-danger material-icons btnClear'>clear</button>" + "</td></tr>");
   } else {
     // Add each bus's data into the table
-    $("#bus-table > tbody").append("<tr><td>" + busName + "</td><td>" + busDest + "</td><td>" +
-      busFreq + "</td><td>" + minutes + "</td><td>" + timeOfNext + "</td></tr>");
+    $("#bus-table > tbody").append("<tr><td>" + busName + "</td><td>" + 
+    busDest + "</td><td>" +
+      busFreq + "</td><td class='minsTo'>" +
+       minutes + "</td><td class='nextTm'>" + 
+       timeOfNext + "</td><td>" + 
+       "<button data=" + snap.key + " class='btn btn-success material-icons btnUpdate'>update</button>" + "</td><td>" +
+       "<button data=" + snap.key + " class='btn btn-danger material-icons btnClear'>clear</button>" + "</td></tr>");
   }
-  $("td").each(function () { $(this).addClass("text-center") })
-  $("th").each(function () { $(this).addClass("text-center") })
+  $("td").each(function () { $(this).addClass("text-center") });
+  $("th").each(function () { $(this).addClass("text-center") });
+}
+
+// Function of Remove button in table (removes the bus from database and html)
+$("#bus-table > tbody").on("click",".btnClear", function(){
+  key=$(this).attr("data");
+  database.ref(key).remove();  
+  $(this).closest("tr").remove();
+})
+
+// Function of Update button in table (updates the bus time in html)
+$("#bus-table > tbody").on("click",".btnUpdate", function(){
+  key=$(this).attr("data");
+  database.ref(key).child('dateChanged').set( firebase.database.ServerValue.TIMESTAMP)  
+  $(this).closest("tr").children(".minsTo").text(minsTo);
+  $(this).closest("tr").children(".nextTm").text(nextTm);
+})
+
+
+// When dateChanged modified bt Update button this is tirggered to update the specific table entries by settingthe global variables minsTo and nextTm using the global variable key (set upon clicking the update button)
+database.ref().on("value", function (snap) {
+  var busName = snap.val()[key].name;
+  var busDest = snap.val()[key].dest;
+  var busFreq = snap.val()[key].freq;
+  var busFirst = snap.val()[key].first;
+  var timeDiff = moment(moment(), "HH:mm").diff(moment(busFirst, "HH:mm"), 'minutes');
+  if (timeDiff < 0) { // if time is past 00:00 (midnight)
+    timeDiff = 24 * 60 + timeDiff - 1;
+  }
+  var minutes = timeDiff % busFreq; // this shows the minutes after previous bus
+  minutes = busFreq - minutes; // subtract from frequency to obtain minutes to next bus
+  var timeOfNext = moment(moment(), "HH:mm").add(minutes, 'minutes').format("HH:mm")
+  //Check if it is past 01:00 am (bus service stops at 01:00)
+  if (moment(timeOfNext, "HH:mm").isBetween(moment("01:00", "HH:mm"), moment(busFirst, "HH:mm"))) {
+      minsTo="No Bus after 1 am";
+      nextTm="First Bus at " + busFirst;
+  } else {
+      minsTo=minutes;
+      nextTm=timeOfNext;
+  }
 
 })
